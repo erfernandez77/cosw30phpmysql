9112bcbeea22166b1b6a08c3581c573a0448424c
EC2 Default User <ec2-user@ip-172-31-85-65.ec2.internal>
2019-08-29 02:58:32 +0000

    Created lab 1 file and added basic html layout

---
+++ cosw30/labwork/lab1/lab1.php
@@ -0,0 +1,18 @@
+<!doctype html>
+    <html>
+        <head>
+            <title>My First PHP Page</title>
+            <style>
+                
+            </style>
+        </head>
+        <body>
+            <h1>Elisa Fernandez</h1>
+            <p>Paragraph about me!</p>
+        
+            <?php
+                echo 'This is my PHP beautiful paragraph';
+            ?>
+        </body>
+    </html>
+    
