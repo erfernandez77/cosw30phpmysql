4585d1efd63efbb06cb1c178b64884ba0bb9be2e
EC2 Default User <ec2-user@ip-172-31-85-65.ec2.internal>
2019-10-11 02:09:26 +0000

    Homework 7 - Function Machine

---
+++ cosw30/homework/hw7/functions.php
@@ -0,0 +1,122 @@
+
+
+<?php
+    $choice = $_POST['choice'];
+    $firstNum = $_POST['firstNum'];
+    $secondNum = $_POST['secondNum'];
+    
+    function outputDis($firstNum, $secondNum){
+        echo "<p>First Number: $firstNum</p>";
+        echo "<p>Second Number: $secondNum</p>";
+    }
+    
+    function multiply($firstNum, $secondNum) {
+        return ($firstNum * $secondNum);
+    }
+    
+    function add($firstNum, $secondNum) {
+        return ($firstNum + $secondNum);
+    }
+    
+    function subtract($firstNum, $secondNum) {
+        return ($firstNum - $secondNum);
+    }
+    
+    function divide($firstNum, $secondNum) {
+        return ($firstNum / $secondNum);
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title>Homework 7 - Function Machine</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="stylesheet.css">
+        <link href="https://fonts.googleapis.com/css?family=Grenze|Josefin+Slab&display=swap" rel="stylesheet"> 
+        
+    </head>    
+    
+    <body>
+        
+        <div id="form-container">
+        
+        <form action="functions.php" method="POST">
+            <h1>Function Machine</h1>
+            
+            <label for="firstNum">Enter first number: 
+                <input type="number" id="firstNum" name="firstNum" required>
+            </label>
+            
+            <br>
+            
+            <label for="secondNum">Enter second number: 
+                <input type="number" id="secondNum" name="secondNum" required>
+            </label>
+            
+            <br>
+            
+            <label>Choose an operator: 
+                <select name="choice" required>
+                    <option diable value="none">-Select Option-</option>
+                    <option value="Multiplication">Multiplication</option>
+                    <option value="Addition">Addition</option>
+                    <option value="Subtraction">Subtraction</option>
+                    <option value="Division">Division</option>
+                </select>
+            </label>
+            
+            <br>
+            
+
+            <input class="btn" type="submit" value="Calculate Now!">
+            
+            <br>
+            
+        
+        
+        <?php 
+            
+            
+            
+            if($choice == "none") {
+                echo "<h3 class=\"warning\">**WARNING! No operator selected. Please try again!</h3>";
+            }
+            
+            elseif ($choice == "Multiplication") {
+                echo outputDis($firstNum, $secondNum);
+                $total =  multiply($firstNum, $secondNum);
+                echo "<h3 class=\"output\">$firstNum * $secondNum = $total</h3>";
+            }
+            
+            elseif ($choice == "Addition") {
+                echo outputDis($firstNum, $secondNum);
+                $total =  add($firstNum, $secondNum);
+                echo "<h3 class=\"output\">$firstNum + $secondNum = $total</h3>";
+            }
+            
+            elseif ($choice == "Subtraction") {
+                echo outputDis($firstNum, $secondNum);
+                $total =  subtract($firstNum, $secondNum);
+                echo "<h3 class=\"output\">$firstNum - $secondNum = $total</h3>";
+            }
+            
+            elseif ($choice == "Division") {
+                echo outputDis($firstNum, $secondNum);
+                $total =  divide($firstNum, $secondNum);
+                echo "<h3 class=\"output\">$firstNum / $secondNum = $total</h3>";
+            }
+        
+        ?>
+        
+        <a class="btn" href="functions.php">Try Again!</a>
+        
+        </form>
+        
+        </div>
+        
+    </body>
+    
+</html>
